372b18ad113db8617bddf77ff9599060a4f1fa73
rnoguer22 <rnogugon@myuax.com>
2022-10-24 10:41:19 +0200

    Class soldier

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,5 +1,20 @@
 # Soldier
 class Soldier:
+    def __init__(self, health, strength):
+        # add code here
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        daño_recibido = self.health - damage
+        if daño_recibido < 0:
+            self.health = 0
+        else:
+            pass
+
 
 # Viking
 class Viking:
