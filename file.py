34e33241b542df2a8d1910a87f2a08f0d4648c34
rnoguer22 <rnogugon@myuax.com>
2022-10-26 11:15:31 +0200

    Elaboramos el juego Pelayo pierde porque maria es la mejor jajaja

--- file.py
+++ file.py
@@ -4,3 +4,13 @@ from vikingsClasses import (
     Viking,
     War
 )
+
+Pelayo = Saxon(100, 99)
+Maria = Viking("Polliqui", 100, 200)
+
+guerra = War()
+guerra.addSaxon(Pelayo)
+guerra.addViking(Maria)
+print(guerra.saxonAttack())
+print(guerra.vikingAttack())
+print(guerra.showStatus())
