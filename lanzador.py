f025a841f47ade0365c83ad7afce721c8e97b32a
rnoguer22 <rnogugon@myuax.com>
2022-10-26 11:16:16 +0200

    lanzador

---
+++ lanzador.py
@@ -0,0 +1,16 @@
+from vikingsClasses import (
+    Soldier,
+    Saxon,
+    Viking,
+    War
+)
+def lanzador():
+    Pelayo = Saxon(100, 99)
+    Maria = Viking("Polliqui", 100, 200)
+
+    guerra = War()
+    guerra.addSaxon(Pelayo)
+    guerra.addViking(Maria)
+    print(guerra.saxonAttack())
+    print(guerra.vikingAttack())
+    print(guerra.showStatus())
